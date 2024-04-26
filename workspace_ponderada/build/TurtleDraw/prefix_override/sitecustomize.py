f34a57be97a015b880b8d70847cd4cf3d9f7a331
Rodrigo-07 <rodrigo.santos@sou.inteli.edu.br>
2024-04-26 11:04:28 -0300

    arquivos iniciais do workspace e package

---
+++ workspace_ponderada/build/TurtleDraw/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/rodrigo-07/Github/Ponderada-M06-S01/workspace_ponderada/install/TurtleDraw'
---
+++ workspace_ponderada/src/TurtleDraw/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'TurtleDraw'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rodrigo-07',
+    maintainer_email='rodrigo.santos@sou.inteli.edu.br',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
